062b1f8735702396b0d2c1a17496f424d4159521
Mark Vilrokx <mvilrokx@gmail.com>
2014-01-05 01:15:31 -0800

    Added support for getting and setting statuses for holster, gun, down and teaser + a status.

--- atthack2014.rb
+++ atthack2014.rb
@@ -9,7 +9,7 @@ require 'haml'
 
 class Atthack2014 < Sinatra::Base
 
-  # enable :sessions
+  enable :sessions
 
   configure do
     set :views, settings.root + '/app/views'
@@ -25,13 +25,89 @@ class Atthack2014 < Sinatra::Base
     # end
   end
 
+  # def self.reset
+  #   session[:teaser]  = 0
+  #   session[:gun]     = 0
+  #   session[:down]    = 0
+  #   session[:holster] = 0
+  # end
+
   get '/' do
+    session[:teaser]  = 0
+    session[:gun]     = 0
+    session[:down]    = 0
+    session[:holster] = 0
     haml :home, :format => :html5
+  end
+
+  get '/holster' do
+    content_type :json
+    {holster: session[:holster]}.to_json
+  end
+
+  get '/gun' do
+    content_type :json
+    {gun: session[:gun]}.to_json
+  end
+
+  get '/down' do
+    content_type :json
+    {down: session[:down]}.to_json
+  end
+
+  get '/teaser' do
+    content_type :json
+    {teaser: session[:teaser]}.to_json
+  end
+
+  get '/holster/:status' do
+    session[:holster] = params[:status]
+    session[:teaser]  = 0
+    session[:gun]     = 0
+    session[:down]    = 0
+    # content_type :json
+    # {holster: session[:holster]}.to_json
+    status 200
+  end
+
+  get '/teaser/:status' do
+    session[:teaser] = params[:status]
+    session[:gun]     = 0
+    session[:down]    = 0
+    session[:holster] = 0
+    status 200
+  end
+
+  get '/gun/:status' do
+    session[:gun] = params[:status]
+    session[:teaser]  = 0
+    session[:down]    = 0
+    session[:holster] = 0
+    status 200
+  end
+
+  get '/down/:status' do
+    session[:down] = params[:status]
+    session[:teaser]  = 0
+    session[:gun]     = 0
+    session[:holster] = 0
+    status 200
   end
 
   get '/status' do
     content_type :json
-    {status: 1}.to_json
+    if session[:holster] == "1"
+      status = 1
+    elsif session[:teaser] == "1"
+      status = 2
+    elsif session[:gun] == "1"
+      status = 3
+    elsif session[:down] == "1"
+      status = 4
+    else
+      status = 0
+    end
+    {status: status}.to_json
   end
 
   # start the server if ruby file executed directly
